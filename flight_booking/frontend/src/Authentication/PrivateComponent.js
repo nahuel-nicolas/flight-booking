ccdd5f2a6b5b575253ec8d85395db0fd48295b97
Nahuel Barbieri <nnahuel012@gmail.com>
2022-04-21 13:28:06 -0300

    privateComponent authentication

--- flight_booking/frontend/src/Authentication/PrivateComponent.js
+++ flight_booking/frontend/src/Authentication/PrivateComponent.js
@@ -1,19 +1,14 @@
-import { Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import AuthContext from './AuthContext'
-import UserHeaderSection from './UserHeaderSection';
 
 const PrivateComponent = ({ children }) => {
+    const navigateTo = useNavigate()
     const { user } = useContext(AuthContext)
     if (user) {
-        return (
-                <>
-                    {/* <UserHeaderSection /> */}
-                    <>{children}</>
-                </>
-            )
+        return <>{children}</>
     } else {
-        return <Navigate to="/login" />
+        navigateTo('/login')
     }
 }
 
